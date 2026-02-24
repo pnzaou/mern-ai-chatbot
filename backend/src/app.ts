5b8f6ff99442c969b071274f1db2137d4df7ecbd
Perrin Emmanuel Nzaou <perrinemmanuelnzaou@gmail.com>
2026-02-24 15:06:41 +0000

    Setup du routage

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,9 +1,19 @@
 import express from "express";
 import { config } from "dotenv";
+import helmet from "helmet";
+import morgan from "morgan";
+import appRouter from "./routes/index.js";
 
 config();
 const app = express()
 
+app.use(helmet({
+    contentSecurityPolicy: false,
+    crossOriginEmbedderPolicy: false
+}));
+app.use(morgan("dev"));
 app.use(express.json());
+
+app.use("/api", appRouter)
 
 export default app;
